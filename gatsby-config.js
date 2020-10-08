d5c761d9ab2c5773721f6cfa9838c7f3f679d3ea
krissdrawing <krx95@o2.pl>
2020-10-08 22:18:00 +0200

    Added icon to manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `KrissDrawing page`,
+    description: `Personal blog`,
+    author: `KrissDrawing`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -53,6 +53,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/assets/images/logo.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
