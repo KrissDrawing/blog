47c805ce10694f8eedffc42561a21514610da053
krissdrawing <krx95@o2.pl>
2020-10-08 22:31:34 +0200

    Added portfolio page placeholder

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation/Navigation";
+
+const Portfolio = () => {
+  return (
+    <>
+      <Navigation />
+      <h2>Tutaj nic nie ma jak coś na razie</h2>
+    </>
+  );
+};
+
+export default Portfolio;
