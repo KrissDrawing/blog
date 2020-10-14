50f85f65686151f5f965860329786a58c0db04ad
krissdrawing <krx95@o2.pl>
2020-10-14 13:24:17 +0200

    Styled color pickers, added worinkg on off switch

--- src/pages/controlls.js
+++ src/pages/controlls.js
@@ -1,7 +1,50 @@
 import { ApolloProvider, gql, useMutation } from "@apollo/client";
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import client from "../gatsby-theme-apollo/client";
 import { HuePicker, AlphaPicker } from "react-color";
+import { FaPowerOff } from "react-icons/fa";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ColorPickers = styled.div`
+  width: 500px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 20px;
+
+  & > p {
+    color: white;
+    margin: 5px;
+  }
+`;
+
+const CustomPointer = styled.div`
+  width: 20px;
+  height: 40px;
+  border: 4px solid white;
+  transform: translateX(-50%);
+  overflow: hidden;
+`;
+
+const PowerButton = styled.button`
+  width: 50px;
+  height: 50px;
+  display: flex;
+  background: none;
+  border: 2px solid ${({ onOff }) => (onOff ? "green" : "black")};
+  color: ${({ onOff }) => (onOff ? "green" : "black")};
+  & > * {
+    margin: auto;
+  }
+  margin: 20px;
+`;
 
 const mutateQueryBrightness = gql`
   mutation setBrightness($brightness: Int = 0) {
@@ -26,11 +69,10 @@ const Controlls = () => {
   const windowGlobal = typeof window !== "undefined" && window;
   const localStorage = windowGlobal.localStorage;
 
-  const [brightness, setBrightness] = useState(0);
-  const [mutateBrightness, { brightnessData }] = useMutation(
-    mutateQueryBrightness
-  );
   const [mutateColor, { colorData }] = useMutation(mutateQueryColor);
+  const [onOff, setOnOff] = useState(true);
+  const [memoryBrightness, setMemoryBrightness] = useState(0.1);
+
   const [lightColor, setLightColor] = useState({
     r: 255,
     g: 0,
@@ -42,14 +84,12 @@ const Controlls = () => {
 
   useEffect(() => {
     colorMemory = JSON.parse(localStorage.getItem("colorMemory"));
-    console.log(colorMemory);
     setLightColor({
       r: colorMemory.r,
       g: colorMemory.g,
       b: colorMemory.b,
       a: colorMemory.a,
     });
-    console.log(lightColor);
   }, []);
   useEffect(() => {
     mutateColor({
@@ -73,31 +113,51 @@ const Controlls = () => {
       b: color.rgb.b,
       a: isAlpha ? color.rgb.a : lightColor.a,
     });
+    color.rgb.a !== 0 ? setOnOff(true) : setOnOff(false);
   };
 
   return (
     <ApolloProvider client={client}>
-      <button
-        onClick={() =>
-          handleChangeComplete({ rgb: { r: 0, g: 0, b: 0, a: 0 } }, true)
-        }
-      >
-        ON/OFF
-      </button>
-      <input
-        value={brightness}
-        onChange={e => setBrightness(e.target.value)}
-        type="number"
-        min="0"
-        max="255"
-      />
-      <button>Set Brightness</button>
-      <HuePicker color={lightColor} onChangeComplete={handleChangeComplete} />
-      <AlphaPicker
-        color={lightColor}
-        onChangeComplete={color => handleChangeComplete(color, true)}
-      />
-      {/* {console.log(brightness)} */}
+      <Wrapper>
+        <PowerButton
+          onOff={onOff}
+          onClick={() => {
+            if (lightColor.a !== 0) setMemoryBrightness(lightColor.a);
+            setOnOff(prev => !prev);
+            handleChangeComplete(
+              {
+                rgb: {
+                  r: lightColor.r,
+                  g: lightColor.g,
+                  b: lightColor.b,
+                  a: onOff === true ? 0 : memoryBrightness,
+                },
+              },
+              true
+            );
+          }}
+        >
+          <FaPowerOff />
+        </PowerButton>
+        <ColorPickers>
+          <p>Pick Color</p>
+          <HuePicker
+            width="100%"
+            height="40px"
+            pointer={CustomPointer}
+            color={lightColor}
+            onChangeComplete={handleChangeComplete}
+          />
+          <p>Pick Brightness: {Math.round(lightColor.a * 100)}%</p>
+          <AlphaPicker
+            color={lightColor}
+            onChangeComplete={color => handleChangeComplete(color, true)}
+            width="100%"
+            height="40px"
+            pointer={CustomPointer}
+          />
+        </ColorPickers>
+      </Wrapper>
     </ApolloProvider>
   );
 };
