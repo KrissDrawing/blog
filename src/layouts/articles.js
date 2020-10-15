03a6e2624db83e505b7b853158870f2c55798976
krissdrawing <krx95@o2.pl>
2020-10-15 12:15:15 +0200

    Changed bearings icon, improved its animation

--- src/layouts/articles.js
+++ src/layouts/articles.js
@@ -5,7 +5,8 @@ import styled from "styled-components";
 import ArticlesList from "./articlesList";
 import PageMenu from "../components/PageMenu/PageMenu";
 import { BiChevronsDown } from "react-icons/bi";
-import { FiSettings } from "react-icons/fi";
+import { GrSettingsOption } from "react-icons/gr";
+import { RiSettings5Fill } from "react-icons/ri";
 import gsap from "gsap/gsap-core";
 
 const Wrapper = styled.div``;
@@ -67,57 +68,17 @@ const BackgroundAnimations = styled.div`
   z-index: -1;
 `;
 
-const BgWrapper1 = styled.div`
-  width: 700px;
-  height: 700px;
+const BgWrapper = styled.div`
   position: absolute;
-  top: 20%;
-  right: 100px;
-  transform: translate(50%, -50%);
-  border-radius: 50%;
-  overflow: hidden;
-  font-size: 300px;
+  left: ${({ x }) => x};
+  top: ${({ y }) => y};
+  transform: translate(-50%, -50%);
+  font-size: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-const BgWrapper2 = styled.div`
-  width: 1000px;
-  height: 1000px;
-  position: absolute;
-  top: 70%;
-  transform: translate(50%, -50%);
-  right: 50%;
-  font-size: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BgWrapper3 = styled.div`
-  width: 500px;
-  height: 500px;
-  position: absolute;
-  top: 80%;
-  right: 10%;
-  transform: translate(50%, -50%);
-  font-size: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BgWrapper4 = styled.div`
-  width: 500px;
-  height: 500px;
-  position: absolute;
-  top: 30%;
-  right: 40%;
-  transform: translate(50%, -50%);
-  font-size: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  color: ${({ color }) => color};
+  opacity: 1;
 `;
 
 export const query = graphql`
@@ -149,21 +110,28 @@ const Articles = ({ data, pageContext }) => {
   const scrollMoreRef = useRef(null);
   const bgAnimRef = useRef(null);
 
+  const Bearings = [
+    { x: "100vw", y: "10vh", color: "#733dfc" },
+    { x: "100vw", y: "30vh", color: "#b93dfc" },
+    { x: "100vw", y: "50vh", color: "#ff3dfc" },
+    { x: "100vw", y: "70vh", color: "#ff3dfc" },
+    { x: "100vw", y: "90vh", color: "#ff3dfc" },
+  ];
+
   useEffect(() => {
     const scrollMore = scrollMoreRef.current;
     const bgAnim = bgAnimRef.current.children;
 
-    for (let i = 0; i <= 3; i++) {
+    for (let i = 0; i <= Bearings.length; i++) {
       gsap.to(bgAnim[i], {
         scrollTrigger: {
           trigger: bgAnim[i],
           start: "top center",
           end: "6000 top",
-          scrub: 1,
+          scrub: i * 0.1 + 1,
           // markers: true,
         },
-        // repeat: -1,
-        rotation: i * 50 + 360,
+        rotation: ((i % 2) - 0.5) * 2 * (i * 50 + 360),
         transformOrigin: "50% 50%",
         ease: "none",
       });
@@ -208,18 +176,11 @@ const Articles = ({ data, pageContext }) => {
       />
       <Background url={data.zigzag.childImageSharp.fixed.src}></Background>
       <BackgroundAnimations ref={bgAnimRef}>
-        <BgWrapper1>
-          <FiSettings />
-        </BgWrapper1>
-        <BgWrapper2>
-          <FiSettings />
-        </BgWrapper2>
-        <BgWrapper3>
-          <FiSettings />
-        </BgWrapper3>
-        <BgWrapper4>
-          <FiSettings />
-        </BgWrapper4>
+        {Bearings.map((item, i) => (
+          <BgWrapper key={i} x={item.x} y={item.y} color={item.color}>
+            <GrSettingsOption />
+          </BgWrapper>
+        ))}
       </BackgroundAnimations>
     </Wrapper>
   );
